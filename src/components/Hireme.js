8af30c91864427b9b76ce33d1c0fa36e720a3fcd
Phi Hung <hungdarker113@gmail.com>
2023-02-15 20:59:27 +0700

    Update Bot

---
+++ src/components/Hireme.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hireme() {
+    return <div>Hireme</div>;
+}
+
+export default Hireme;
---
+++ src/components/Skills.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SKills() {
+    return <div>SKills</div>;
+}
+
+export default SKills;
